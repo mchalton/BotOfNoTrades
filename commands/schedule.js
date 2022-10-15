844b44b849bb29036242ae0f619baf42805a560f
Matt Chalton <mattchalton@gmail.com>
2022-10-15 11:50:18 +0100

    changed title url to connect url

--- commands/schedule.js
+++ commands/schedule.js
@@ -70,7 +70,7 @@ module.exports = {
 		var mainEmbed = new MessageEmbed()
 			.setColor('0xFF6F00')
 			.setTitle('10 Man')
-			.setURL('https://10man.commoncrayon.com/')
+			.setURL('steam://connect/54.37.198.140:27015/jont')
 			.setDescription('Join a 10 Man!')
 			.addFields(
 				{ name: 'Time:', value: `<t:${epochTime}>`},
@@ -276,7 +276,7 @@ function createEmbed(yesString, noString, timeScheduled, yesEntry, noEntry) {
 	var mainEmbed = new MessageEmbed()
 		.setColor('0xFF6F00')
 		.setTitle('10 Man')
-		.setURL('https://10man.commoncrayon.com/')
+		.setURL('steam://connect/54.37.198.140:27015/jont')
 		.setDescription('Join a 10 Man!')
 		.addFields(
 			{ name: 'Time:', value: `<t:${epochTime}>` },
